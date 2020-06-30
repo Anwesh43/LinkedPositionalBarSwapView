c9f08a0597ddff539f4b6bd26f5b30512fd30389
Anwesh43 <anweshthecool0@gmail.com>
2020-07-01 02:13:13 +0530

    adding PositionalBarSwapView class

--- positionalbarswapview/src/main/java/com/anwesh/uiprojects/positionalbarswapview/PositionalBarSwapView.kt
+++ positionalbarswapview/src/main/java/com/anwesh/uiprojects/positionalbarswapview/PositionalBarSwapView.kt
@@ -51,3 +51,19 @@ fun Canvas.drawPBSNode(i : Int, scale : Float, paint : Paint) {
     paint.color = Color.parseColor(colors[i])
     drawPositionalBarSwap(scale, w, h, paint)
 }
+
+class PositionalBarSwapView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
